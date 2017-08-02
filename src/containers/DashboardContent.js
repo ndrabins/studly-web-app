d62833b5c09d4ae32c74dfdfce7737748fc158b2
Noah Drabinsky <noah.drabinsky1@gmail.com>
2017-08-02 11:41:32 -0400

    Assignments load into calendar, still have a bug on first load tho

--- src/containers/DashboardContent.js
+++ src/containers/DashboardContent.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import * as Actions from "../actions";
+import moment from "moment";
+import Map from "lodash/map";
+import Flatten from "lodash/flatten";
 
-import Calendar from '../components/Calendar';
+import Calendar from "../components/Calendar";
 
 const styles = {
   container: {
@@ -15,31 +18,49 @@ const styles = {
     margin: "20px",
     height: "100%"
   },
-  dayContainer : {
+  dayContainer: {
     backgroundColor: "#EEEEEE",
-    flex:1,
-    height:"100%",
-    border:"1px solid black",
-    margin:"3px"
-
+    flex: 1,
+    height: "100%",
+    border: "1px solid black",
+    margin: "3px"
   }
-}
+};
 
 class DashboardContent extends Component {
+  getEvents() {
+    let allAssignments = this.props.assignments;
+
+    let events = [];
+
+    //formatting for calendar
+    let assignmentEvents = Map(
+      allAssignments,
+      (courseAssignments, courseKey) => {
+        let eventsForCourse = Map(
+          courseAssignments,
+          (assignment, assignmentKey) => {
+            assignment["title"] = assignment["assignmentTitle"];
+            let dueDate = moment(assignment.dueDate).toDate();
+            let endDate = moment(assignment.dueDate).add(1, "minute").toDate();
+            assignment["dueDate"] = dueDate;
+            assignment["endDate"] = endDate;
+            return assignment;
+          }
+        );
+        return eventsForCourse;
+      }
+    );
+    return Flatten(assignmentEvents);
+  }
+
   render() {
     return (
       <div style={styles.container}>
         <h1>Welcome to Studly</h1>
         <h4>Here is your schedule for the week</h4>
         <div style={styles.calendarContainer}>
-          {/* <div style={styles.dayContainer }></div>
-          <div style={styles.dayContainer }></div>
-          <div style={styles.dayContainer }></div>
-          <div style={styles.dayContainer }></div>
-          <div style={styles.dayContainer }></div>
-          <div style={styles.dayContainer }></div>
-          <div style={styles.dayContainer }></div> */}
-          <Calendar />
+          <Calendar events={this.getEvents()} />
         </div>
       </div>
     );
@@ -51,7 +72,7 @@ function mapStateToProps(state) {
     authenticated: state.auth.authenticated,
     courses: state.courses.data,
     sideNavOpen: state.utility.sideNavOpen,
-
+    assignments: state.assignments.data
   };
 }
 
